d999c8700c712e60c1dd40fbba135e248f81ca59
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2023-01-26 11:38:59 +0200

    Добавление второй лабы

--- practice/2_pract/main.cpp
+++ practice/2_pract/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-#include <vector>
 int main() {
-    const int N = 1000;
-    int firstMas[N] = {1, 2 , 3, 4, 5};
-    int secondMas[N] = {};
-    if (sizeof(firstMas[0]) <= 8){
-        for (int i = 0; i < N; i++){
-            secondMas[i] = firstMas[i];
-        }
+    const int N = 20;
+    char* firstMas = new char[N] {1, 2, 3, 4, 5, 3, 4, 5, 3, 4, 5, 3, 4, 5, 3, 4, 5, 3, 4, 5};
+    char* secondMas = new char[N];
+
+
+    const int n = 10;
+    //int p = n;
+    auto* newFirstMas = reinterpret_cast<int*>(&n);
+    auto* newSecondMas = (size_t*)secondMas;
+
+    for (int i = 0; i < (int)(N / sizeof(size_t)); i++){
+        newSecondMas[i] = newFirstMas[i];
     }
-    else{
-        secondMas[0] = static_cast<int*>(firstMas);
+
+    firstMas = (char*)newFirstMas;
+    secondMas = (char*)newSecondMas;
+
+    for (int i = (int)(N / sizeof(size_t)); i < N; i++){
+        secondMas[i] = firstMas[i];
+    }
+    for (int i = 0; i < N; i++){
+        std::cout << (int)(secondMas[i]) << " ";
     }
 }
