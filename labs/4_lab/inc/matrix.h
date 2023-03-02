9ad27e8c85631a29089aba4a331fa98036450dc1
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2023-03-02 20:00:26 +0200

    готова 4-ая лабораторная

--- labs/4_lab/inc/matrix.h
+++ labs/4_lab/inc/matrix.h
@@ -5,28 +5,29 @@
 #include <vector>
 
 template<typename type, unsigned short N, unsigned short M>
-
-
-
 class matrix {
 private:
     type m_matrix[N][M];
+
     template<typename type, unsigned short N, unsigned short M>
     friend std::ostream& operator<<(std::ostream& out, const matrix<type, N, M> &current);
+
     template<typename type, unsigned short N, unsigned short M>
     friend std::istream& operator>> (std::istream& in, matrix<type, N, M>& current);
 public:
-    matrix() = default;
+    matrix() { std::cout << "constructor" << std::endl; };
+
     matrix(const matrix &current) {
-        std::cout << 2 << std::endl;
+        std::cout << "--- copy constructor" << std::endl;
         for (int i = 0; i < N; i++){
             for (int j = 0; j < M; j++){
                 m_matrix[i][j] = current.m_matrix[i][j];
             }
         }
     }
+
     matrix& operator= (const matrix<type, N, M>& current){
-        std::cout << 3 << std::endl;
+        std::cout << "=== assignment constructor" << std::endl;
         if (this != &current){
             for (int i = 0; i < N; i++){
                 for (int j = 0; j < M; j++){
@@ -36,18 +37,68 @@ public:
         }
         return *this;
     }
+
     type determinant(){
-        if (N == M == 2){
+
+        if (N == M && M == 2){
             return static_cast<type>(m_matrix[0][0] * m_matrix[1][1] - m_matrix[1][0] * m_matrix[0][1]);
         }
-        else if (N == M == 3){
+        else if (N == M && M == 3){
             return static_cast<type>(m_matrix[0][0] * m_matrix[1][1] * m_matrix[2][2] + m_matrix[1][0] * m_matrix[0][2] * m_matrix[2][1] + m_matrix[0][1] * m_matrix[2][0] * m_matrix[1][2] - m_matrix[0][2] * m_matrix[1][1] * m_matrix[2][0] - m_matrix[0][0] * m_matrix[1][2] * m_matrix[2][1] - m_matrix[2][2] * m_matrix[0][1] * m_matrix[1][0]);
         }
-        else if (N == M == 1){
+        else if (N == M && M == 1){
             return static_cast<type>(m_matrix[0][0]);
         }
     }
-    ~matrix()= default;
+
+    matrix& operator+= (const matrix<type, N, M>& rightMatrix){
+            for (int i = 0; i < N; i++){
+                for (int j = 0; j < M; j++){
+                    m_matrix[i][j] += rightMatrix.m_matrix[i][j];
+                }
+            }
+        return *this;
+    }
+    matrix operator+ (const matrix<type, N, M>& rightMatrix){
+        matrix<type, N, M> tempMatrix = *this;
+        tempMatrix += rightMatrix;
+        return tempMatrix;
+    }
+
+    matrix& operator*= (const matrix<type, M, N>& rightMatrix){
+        matrix<type, N, M> tempMatrix = *this;
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++){
+                m_matrix[i][j] = 0;
+                for (int u = 0; u < M; u++){
+                    m_matrix[i][j] += tempMatrix[i][u] * rightMatrix[u][j];
+                }
+            }
+        }
+        return *this;
+    }
+    matrix operator* (const matrix<type, M, N>& rightMatrix){
+        matrix<type, N, M> tempMatrix = *this;
+        tempMatrix *= rightMatrix;
+        return tempMatrix;
+    }
+
+    matrix& operator++ (){
+        for (int i = 0; i < N; i++){
+            for (int j = 0; j < M; j++){
+                m_matrix[i][j]++;
+            }
+        }
+    }
+
+    type& at(unsigned int firstIndex, unsigned int secondIndex){
+        if (firstIndex < N && secondIndex < M){
+            return m_matrix[firstIndex][secondIndex];
+        }
+        std::abort();
+
+    }
+    ~matrix() {std::cout << "destructor" << std::endl;};
 };
 
 template<typename type, unsigned short N, unsigned short M>
--- labs/4_lab/main.cpp
+++ labs/4_lab/main.cpp
@@ -2,11 +2,14 @@
 #include <matrix.h>
 
 int main() {
-    matrix<int, 2, 2> firstMatrix;
+    matrix<int, 2, 2> firstMatrix{};
     std::cin >> firstMatrix;
-    std::cout << firstMatrix.determinant() << std::endl;
+    std::cout << "Determinant = " << firstMatrix.determinant() << std::endl;
     std::cout << firstMatrix;
 
-    matrix<int, 2, 2> secondMatrix = firstMatrix;
-    std::cout << secondMatrix;
+    matrix<int, 2, 2> secondMatrix{};
+    secondMatrix = firstMatrix;
+    std::cout << firstMatrix.at(0, 0) << std::endl;
+    std::cout << firstMatrix + secondMatrix << std::endl;
+
 }
