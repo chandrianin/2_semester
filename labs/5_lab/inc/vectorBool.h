777c2ceec12798778901181fdd4bf3e358f47704
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2023-03-04 02:47:18 +0200

    + конструктор + чтение и изменение элементов

--- labs/5_lab/inc/vectorBool.h
+++ labs/5_lab/inc/vectorBool.h
@@ -3,7 +3,37 @@
 //
 
 #pragma once
+namespace vb {
+    template<typename type>
+    class vector {
 
-class vectorBool {
+    };
 
-};
+    template<>
+    class vector<bool> {
+    private:
+        unsigned short length{0};
+        char* boolMas = new char[0];
+    public:
+        vector<bool>(){}
+        bool operator[](unsigned int index) const{
+            return static_cast<bool>(((boolMas[index / 8]) >> index) & 0b00000001);
+        }
+        void edit(int index, bool value){
+            char degree = 1;
+            for (int i = 0; i < index; i++){
+                degree *= 2;
+            }
+            value ? boolMas[index / 8] |= degree : boolMas[index / 8] &= ~degree;
+        }
+        void push_back(bool value){
+            //TODO доделать
+//            length += 1;
+//            char* temp;
+//            length % 8 == 0 ? temp = new char[1 + (length / 8)] : temp = boolMas;
+
+        }
+    };
+
+
+}
---
+++ labs/5_lab/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vectorBool.h>
+
+int main() {
+    vb::vector<bool> firstBoolVector;
+
+    return 0;
+}
