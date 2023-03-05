399f77b1ca6dd76096ff66ab1746e78bc8119e2b
TbMA13 <74298344+TbMA13@users.noreply.github.com>
2023-03-05 02:00:56 +0200

    сделана 5-ая лаба

--- labs/5_lab/inc/vectorBool.h
+++ labs/5_lab/inc/vectorBool.h
@@ -1,7 +1,7 @@
 //
 // Created by TbMA3 on 04.03.2023.
 //
-
+#include <iostream>
 #pragma once
 namespace vb {
     template<typename type>
@@ -12,14 +12,14 @@ namespace vb {
     template<>
     class vector<bool> {
     private:
-        unsigned short length{0};
+        size_t length{0};
         char* boolMas = new char[0];
     public:
-        vector<bool>(){}
+        vector<bool>() = default;
         bool operator[](unsigned int index) const{
             return static_cast<bool>(((boolMas[index / 8]) >> index) & 0b00000001);
         }
-        void edit(int index, bool value){
+        void edit(size_t index, bool value){
             char degree = 1;
             for (int i = 0; i < index; i++){
                 degree *= 2;
@@ -27,11 +27,46 @@ namespace vb {
             value ? boolMas[index / 8] |= degree : boolMas[index / 8] &= ~degree;
         }
         void push_back(bool value){
-            //TODO доделать
-//            length += 1;
-//            char* temp;
-//            length % 8 == 0 ? temp = new char[1 + (length / 8)] : temp = boolMas;
+            if (length % 8 == 0){
+                char* temp = new char[(length / 8) + 1];
+                for (int i = 0; i < length; i++){
+                    temp[i] = boolMas[i];
+                }
+                delete[] boolMas;
+                boolMas = temp;
+                edit(length, value);
+            }
+            else{
+                edit(length, value);
+            }
+            length++;
+        }
+        [[nodiscard]] size_t size() const{
+            return length;
+        }
+        void insert(size_t index, bool  value){
+            this->push_back(operator[](length - 1));
+            for (size_t i = length; i != index; i--)
+                edit(i, operator[](i - 1));
 
+            edit(index, value);
+        }
+        void erase(size_t firstIndex, size_t lastIndex){
+            for (size_t i = lastIndex; i < length; i++){
+                edit(i - (lastIndex - firstIndex), operator[](i));
+//                boolMas[i - (lastIndex - firstIndex)] = boolMas[i];
+            }
+            length -= lastIndex - firstIndex;
+        }
+        void erase(size_t index){
+            for (size_t i = index; i < length - 1; i++){
+                edit(i, operator[](i + 1));
+//                boolMas[i] = boolMas[i + 1];
+//                for(int j = 0; j < this->length; j++){
+//                    std::cout << boolMas[j] << " ";
+//                }
+            }
+            length--;
         }
     };
 
--- labs/5_lab/main.cpp
+++ labs/5_lab/main.cpp
@@ -2,7 +2,25 @@
 #include <vectorBool.h>
 
 int main() {
-    vb::vector<bool> firstBoolVector;
+    vb::vector<bool> boolVector;
+    boolVector.push_back(true);
+    boolVector.push_back(false);
+    boolVector.push_back(true);
+    boolVector.push_back(false);
 
+    // работает
+    //boolVector.insert(3, true);
+
+    // работает
+    //boolVector.edit(1, true);
+
+    // работает
+    //boolVector.erase(3);
+
+    // работает
+    //boolVector.erase(1, 4);
+    for(int i = 0; i < boolVector.size(); i++){
+        std::cout << boolVector[i] << " ";
+    }
     return 0;
 }
